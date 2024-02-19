c9b4692d96738f26a8e5d2df7a8e48e15a6249d3
Naveencodespeaks <adepusainaveen.codespeaks@gmail.com>
2024-02-19 13:35:02 +0530

    add the test_script.py

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,21 +1,21 @@
-
 import unittest
-
 
 from main import app
 import os
 
-class TestToPerform(uniitest.TestCase):
-    def setup(self):
+
+class TestToPerform(unittest.TestCase):
+    def setUp(self):
         self.app = app.test_client()
 
-    def tearDown():
+    def tearDown(self):
         pass
 
-    def test_age(self):
-        response = self.app.get('/',follow_redirects = True)
+    def test_page(self):
+        response = self.app.get('/', follow_redirects=True)
         print(response)
         self.assertEqual(response.status_code, 200)
 
-if __name__=='__main__':
-    uniitest.main()
+
+if __name__ == '__main__':
+    unittest.main()
